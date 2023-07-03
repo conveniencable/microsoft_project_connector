a860cc338254f28d5c96e6e67c2f66a1354531ae
Li Chan <conveniencable@outlook.com>
2023-07-03 22:24:14 +0800

    V1.0.13

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :microsoft_project_connector do
   name 'Microsoft Project Connector'
   author 'Li Chan'
   description 'A Redmine Plugin to Work with MS Project'
-  version '1.0.12'
+  version '1.0.13'
   url 'https://github.com/conveniencable/microsoft_project_connector'
   author_url 'https://github.com/conveniencable'
 
